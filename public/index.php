7574c8d6cb5e13f1d861b0bf7c1be0b93d0f2d61
vKUR0 <cecille@et.esiea.fr>
2025-02-19 18:27:26 +0100

    fixed footer+acceuil

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php include '../views/header.php'; ?>
 <div class="container mt-5">
     <h1 class="text-center">Bienvenue sur notre plateforme de réservation</h1>
-    <p class="text-center">Prenez vos rendez-vous en toute simplicité.</p>
 </div>
 <?php include '../views/footer.php'; ?>
