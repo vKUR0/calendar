c1d44f336378e4fe33a61b2b8397ee810f4a6d32
vKUR0 <cecille@et.esiea.fr>
2025-02-27 09:46:23 +0100

    removed footer

--- views/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="bg-light text-center py-3 mt-4 fixed-bottom">
-        <p>&copy; 2025 Revervation</p>
-    </footer>
-    <script src="../assets/bootstrap.bundle.min.js"></script>
-    <script src="../assets/script.js"></script>
-</body>
-</html>
